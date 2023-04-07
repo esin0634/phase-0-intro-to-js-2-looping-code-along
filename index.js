790314fbdf1bfbf11353b500215863216f110e4f
Esin <nbusra.esin@gmail.com>
2023-04-07 18:00:48 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(array, eventName){
+    let newArr = []
+    for(let i = 0; i < array.length ; i += 1){
+        newArr.push("Thank you, " + array[i] + ", for the wonderful "  + eventName + " gift!")
+
+    }
+    return newArr
+}
+
+function countDown(num){
+    while (num >= 0){
+        console.log(num)
+        num -= 1 
+    }
+}
+
